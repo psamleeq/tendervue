723a98c49136c83c09cab414dad5a2d401dbdff3
lancelin <lancelin@bim-group.com>
2022-12-06 11:43:05 +0800

    change [日報表-PI3.1附件] 新增「頁數調整」

--- src/views/PI/perfReport/PI3_1_Att.vue
+++ src/views/PI/perfReport/PI3_1_Att.vue
@@ -50,6 +50,13 @@
 						<el-form-item label="起始頁碼">
 							<el-input-number v-model="initPage" controls-position="right" :min="1" @change="setPDFinputs" />
 						</el-form-item>
+						<el-form-item label="頁數調整">
+							<el-button-group>
+								<el-button type="success" size="small" icon="el-icon-plus" @click="addPage" />
+								<el-button type="danger" size="small" icon="el-icon-minus" :disabled="template.schemas != undefined && template.schemas.length <= 2" @click="removePage" />
+								<span v-if="template.schemas != undefined" style="margin-left: 5px">(目前頁數: {{ template.schemas.length }})</span>
+							</el-button-group>
+						</el-form-item>
 					</el-form>
 				</el-card>
 			</el-col>
@@ -62,6 +69,7 @@
 import moment from "moment";
 import { generate } from '@pdfme/generator';
 import { Form } from '@pdfme/ui';
+import { PDFDocument } from 'pdf-lib'
 // import { getCaseCount } from "@/api/PI";
 
 export default {
@@ -163,6 +171,65 @@ export default {
 				// this.getList();
 			})
 		},
+		async addPage() {
+			this.loading = true;
+
+			//Step1: 合併PDF
+			const ori_pdfUint8 = Uint8Array.from(window.atob(this.template.basePdf.replace(/^data:application\/pdf;base64,/, '')), c => c.charCodeAt(0));
+			const ori_pdf = await PDFDocument.load(ori_pdfUint8.buffer);
+
+			const addTemplate = await fetch(`/assets/pdf/PI3-1-1.json?t=${Date.now()}`).then(response => response.json());
+			const add_pdfUint8 = Uint8Array.from(window.atob(addTemplate.basePdf.replace(/^data:application\/pdf;base64,/, '')), c => c.charCodeAt(0));
+			const add_pdf = await PDFDocument.load(add_pdfUint8.buffer);
+			// const addPdfBytes = await fetch(`/assets/pdf/PI3-1-1.pdf?t=${Date.now()}`).then(res => res.arrayBuffer());
+			// const add_pdf = await PDFDocument.load(addPdfBytes);
+			const mergedPdf = await PDFDocument.create();
+
+			const ori_copiedPages = await mergedPdf.copyPages(ori_pdf, ori_pdf.getPageIndices());
+			const [ add_copiedPage ] = await mergedPdf.copyPages(add_pdf, [0]);
+			ori_copiedPages.forEach(page => mergedPdf.addPage(page));
+			mergedPdf.insertPage(ori_pdf.getPageCount()-1, add_copiedPage);
+
+			// const mergedPdfFile = await mergedPdf.save();
+			// const blob = new Blob([mergedPdfFile.buffer], { type: 'application/pdf' });
+			// window.open(URL.createObjectURL(blob));
+			this.template.basePdf = await mergedPdf.saveAsBase64({ dataUri: true });
+
+			//Step2: 調整欄位
+			const lastIndex = this.template.schemas.length-1;
+			addTemplate.schemas[0][`serialNumber${lastIndex+1}`] = addTemplate.schemas[0].serialNumber1; 
+			delete addTemplate.schemas[0].serialNumber1;
+			addTemplate.schemas[0][`checkImg${lastIndex+1}`] = addTemplate.schemas[0].checkImg1;
+			delete addTemplate.schemas[0].checkImg1;
+			this.template.schemas.splice(lastIndex, 0, addTemplate.schemas[0]);
+
+			this.setTemplate();
+		},
+		async removePage() {
+			this.loading = true;
+
+			//Step1: 移除頁面
+			const ori_pdfUint8 = Uint8Array.from(window.atob(this.template.basePdf.replace(/^data:application\/pdf;base64,/, '')), c => c.charCodeAt(0));
+			const ori_pdf = await PDFDocument.load(ori_pdfUint8.buffer);
+			const lastIndex = ori_pdf.getPageCount()-2;
+			ori_pdf.removePage(lastIndex);
+
+			this.template.basePdf = await ori_pdf.saveAsBase64({ dataUri: true });
+
+			//Step2: 調整欄位
+			this.template.schemas.splice(lastIndex, 1);
+			console.log(this.template.schemas);
+			this.setTemplate();
+		},
+		setTemplate() {
+			const lastIndex = this.template.schemas.length-1;
+			const key_SN = Object.keys(this.template.schemas[lastIndex]).filter(key => key.includes("serialNumber"))[0];
+			this.template.schemas[lastIndex][`serialNumber${lastIndex+1}`] = this.template.schemas[lastIndex][key_SN]; 
+			delete this.template.schemas[lastIndex][key_SN];
+
+			this.form.updateTemplate(this.template);
+			this.setPDFinputs();
+		},
 		setPDFinputs() {
 			const date = moment(this.searchDate).subtract(1911, 'year');
 			this.inputs.date = date.format("YYYY年MM月DD日").slice(1);
@@ -179,6 +246,7 @@ export default {
 
 			this.form.setInputs([this.inputs]);
 			this.form.render();
+			this.loading = false;
 		},
 		// getList() {
 		// 	this.loading = true;
