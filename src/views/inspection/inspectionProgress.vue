a3249ad05f7ad00dcbe397f6e9de34aa2fe7360a
ftech-hannah <hannah@bim-group.com>
2023-05-31 10:21:45 +0800

    fix [巡查歷程] 空格修正成tab

--- src/views/inspection/inspectionProgress.vue
+++ src/views/inspection/inspectionProgress.vue
@@ -2,52 +2,50 @@
 	<div class="app-container" v-loading="loading">
 	<h2>巡查歷程</h2>
 	<div class="filter-container">
-      <div class="filter-item">
-        <div
-          class="el-input el-input--medium el-input-group el-input-group--prepend"
-        >
-          <div class="el-input-group__prepend">
-            <span>行政區</span>
-          </div>
-          <el-select
-            v-model.number="listQuery.administrativeArea"
-            placeholder="請選擇"
-            popper-class="type-select"
-            style="width: 100px"
-          >
-            <el-option
-              v-for="(name, id) in area"
-              :key="id"
-              :value="Number(id)"
-              :label="name"
-            />
-          </el-select>
-        </div>
-      </div>
-	  <div class="filter-item">
-        <div
-          class="el-input el-input--medium el-input-group el-input-group--prepend"
-        >
-          <div class="el-input-group__prepend">
-            <span>收取日</span>
-          </div>
-          <el-date-picker
-            v-model="searchDate"
-            style="width: 200px"
-            type="date"
-            placeholder="收取日期"
-            :clearable="false"
-          />
-        </div>
-      </div>
-      <el-button
-        class="filter-item"
-        type="primary"
-        icon="el-icon-search"
-        @click="getList()"
-        >搜尋</el-button
-      >
-    </div>
+		<div class="filter-item">
+			<div class="el-input el-input--medium el-input-group el-input-group--prepend">
+				<div class="el-input-group__prepend">
+				<span>行政區</span>
+				</div>
+				<el-select
+				v-model.number="listQuery.administrativeArea"
+				placeholder="請選擇"
+				popper-class="type-select"
+				style="width: 100px"
+				>
+				<el-option
+					v-for="(name, id) in area"
+					:key="id"
+					:value="Number(id)"
+					:label="name"
+				/>
+				</el-select>
+			</div>
+		</div>
+		<div class="filter-item">
+		<div
+			class="el-input el-input--medium el-input-group el-input-group--prepend"
+		>
+			<div class="el-input-group__prepend">
+			<span>收取日</span>
+			</div>
+			<el-date-picker
+			v-model="listQuery.recieveDate"
+			style="width: 200px"
+			type="date"
+			placeholder="收取日期"
+			:clearable="false"
+			/>
+		</div>
+		</div>
+		<el-button
+		class="filter-item"
+		type="primary"
+		icon="el-icon-search"
+		@click="getList()"
+		>搜尋</el-button
+		>
+	</div>
 	<el-table
 		ref="multipleTable"
 		empty-text="目前沒有資料"
@@ -99,7 +97,6 @@ export default {
 	name: "inspectionProgress",
 	data() {
 	return {
-		searchDate: '',
 		loading: false,
 		listQuery: {
 			administrativeArea:'',
@@ -120,26 +117,26 @@ export default {
 			}
 		],
 		headers:{
-		Id:{
-			name:'編號',
-			sortable: false,
-		},
-		Administrative:{
-			name:'行政區',
-			sortable: false,
-		},
-		ReceiveDate:{
-			name:'收取日',
-			sortable: false,
-		},
-		CompleteDate:{
-			name:'完成日',
-			sortable: false,
-		},
-		Note:{
-			name:'備註',
-			sortable: false,
-		},
+			Id:{
+				name:'編號',
+				sortable: false,
+			},
+			Administrative:{
+				name:'行政區',
+				sortable: false,
+			},
+			ReceiveDate:{
+				name:'收取日',
+				sortable: false,
+			},
+			CompleteDate:{
+				name:'完成日',
+				sortable: false,
+			},
+			Note:{
+				name:'備註',
+				sortable: false,
+			},
 		}
 	};
 	},
@@ -149,8 +146,12 @@ export default {
 	},
 	mounted() {},
 	methods: {
+		formatTime(time) {
+			return time ? moment(time).format("YYYY-MM-DD") : "";
+		},
 		getList(){
-			console.log('getList');
+			const recieveDate = this.formatTime(this.listQuery.recieveDate);
+			console.log(recieveDate);
 		},
 		setComplete(){
 
