d7e0a75e5f9faf55c19043cb4f6d32176d4ebf57
lancelin <lancelin@bim-group.com>
2023-06-13 18:44:54 +0800

    change [巡查歷程] 調整UI/UX

--- src/views/inspection/inspectionProgress.vue
+++ src/views/inspection/inspectionProgress.vue
@@ -6,7 +6,7 @@
 			<div class="filter-item">
 				<div class="el-input el-input--medium el-input-group el-input-group--prepend">
 					<div class="el-input-group__prepend">
-					<span>行政區</span>
+						<span>行政區</span>
 					</div>
 					<el-select
 						v-model.number="listQuery.ZipCode"
@@ -24,19 +24,8 @@
 				</div>
 			</div>
 			<div class="filter-item">
-				<div class="el-input el-input-group el-input-group--prepend">
-					<div class="el-input-group__prepend">
-						<span>收取日</span>
-					</div>
-					<el-date-picker
-						v-model="listQuery.searchDate"
-						style="width: 300px"
-						type="daterange"
-						start-placeholder="開始日期"
-						end-placeholder="結束日期"
-						:clearable="false"
-					/>
-				</div>
+				<div style="font-size: 12px; color: #909399">收取日</div>
+				<time-picker class="filter-item" :shortcutType="'day'" :timeTabId.sync="timeTabId" :dateRange.sync="listQuery.searchDate" @search="getList"/>
 			</div>
 			<el-button class="filter-item" type="primary" icon="el-icon-search" @click="getList()">搜尋</el-button>
 		</div>
@@ -45,12 +34,18 @@
 		<el-table ref="multipleTable" empty-text="目前沒有資料" :data="list" border fit :header-cell-style="{ 'background-color': '#F2F6FC' }" style="width: 100%">
 			<el-table-column v-for="(value, key) in headers" :key="key" :prop="key" :label="value.name" align="center" :sortable="value.sortable">
 				<template slot-scope="{ row, column }">
-					<span v-if="['DateCompleted_At'].includes(column.property)">
-						<span style="display:inline-block;margin-right:10px">{{ formatTime(row[column.property])}}</span>
-						<el-button type="success" size="mini" @click="showDialog(row)">完成</el-button>
+					<span v-if="['State'].includes(column.property)">
+						<span v-if="row.DateCompleted_At">
+							{{ row.DateCompleted_With_Name }} 於 {{ formatTime(row.DateCompleted_At)}} 備註完成
+						</span>
+						<span v-else-if="row.Duty_with">
+							{{ row.Duty_with_Name }} 標記中
+						</span>
+						<span v-else> - </span>
+						<el-button v-if="checkPermission(['inspection.marker']) && row.DateCompleted_At" type="danger" size="mini" round plain @click="showDialog(row, -1)">撤銷</el-button>
 					</span>
 					<span v-else-if="['DateCreate_At'].includes(column.property)">
-						<span>{{ formatTime(row[column.property])}}</span>
+						<span>{{ formatTime(row[column.property]) }}</span>
 					</span>
 					<span v-else-if="['Notes'].includes(column.property)">
 						<span v-if="row.isEdit">
@@ -73,19 +68,23 @@
 			<el-table-column v-if="checkPermission(['inspection.marker'])" label="操作" align="center">
 				<template slot-scope="{ row }">
 					<el-button-group>
-						<el-button class="btn-action" type="primary" plain size="mini" round @click="showMap(row)">標記</el-button>
+						<el-button v-if="!row.DateCompleted_At" class="btn-action" type="primary" plain size="mini" round @click="showMap(row)">標記</el-button>
 						<el-button class="btn-action" type="info" plain size="mini" round @click="showList(row)">列表</el-button>
+						<el-button v-if="!row.DateCompleted_At" type="success" size="mini" round @click="showDialog(row, 2)">完成</el-button>
 					</el-button-group>
 				</template>
 			</el-table-column>
 		</el-table>
 
-		<!-- 完成 對話框 -->
-		<el-dialog title="完成日" :visible.sync="dialogVisible" width="400px">
-			<span>是否確定提交完成日?</span>
+		<!-- 確認 對話框 -->
+		<el-dialog title="確認" :visible.sync="dialogVisible" width="400px">
+			<span>是否確定
+				<span v-if="rowActive.state == -1" style="color: #F56C6C">撤銷</span>
+				<span v-else-if="rowActive.state == 2" style="color: #409EFF">提交</span> 完成?
+			</span>
 			<span slot="footer" class="dialog-footer">
 				<el-button @click="dialogVisible = false">取消</el-button>
-				<el-button type="primary" @click="dialogVisible = false;setComplete()">確定</el-button>
+				<el-button type="primary" @click="dialogVisible = false; setResult();">確定</el-button>
 			</span>
 		</el-dialog>
 	</div>
@@ -95,13 +94,16 @@
 import moment from "moment";
 import { getInspectionList, setInspectionList } from "@/api/inspection";
 import checkPermission from '@/utils/permission';
+import TimePicker from '@/components/TimePicker';
 
 export default {
 	name: "inspectionProgress",
+	components: { TimePicker },
 	data() {
 	return {
 		loading: false,
 		dialogMapVisible: true,
+		timeTabId: 1,
 		listQuery: {
 			ZipCode:0,
 			searchDate:[],
@@ -126,36 +128,40 @@ export default {
 				name:'收取日',
 				sortable: false,
 			},
-			DateCompleted_At:{
-				name:'完成日',
-				sortable: false,
-			},
 			Notes:{
 				name:'備註',
 				sortable: false,
 			},
+			State:{
+				name:'狀態',
+				sortable: false,
+			}
 		},
 		dialogVisible: false,
 		InputNotes:''
 	};
 	},
 	computed: {},
-	watch: { },
+	watch: {},
 	created() {
 		// 設置收取日的默認值為昨日
 		const yesterday = new Date();
 		yesterday.setDate(yesterday.getDate() - 1);
 		this.listQuery.searchDate = [yesterday, yesterday]; 
 	},
-	mounted() {
-	},
+	mounted() {},
 	methods: {
 		checkPermission,
 		showMap(row) {
-			this.$router.push({
-				path: "/inspection/caseMark",
-				query: { inspectId: row.InspectId, caseInspectId: row.InspectId },
-			});
+			this.rowActive = JSON.parse(JSON.stringify(row));
+			this.$set(this.rowActive, "state", 1);
+
+			this.setResult().then(() => {
+				this.$router.push({
+					path: "/inspection/caseMark",
+					query: { inspectId: row.InspectId, caseInspectId: row.InspectId },
+				});
+			})
 		},
 		showList(row) {
 			this.$router.push({
@@ -205,14 +211,10 @@ export default {
 				}).catch(err => {this.loading = false});
 			}
 		},
-		showDialog(row){
+		showDialog(row, state){
 			this.dialogVisible=true;
 			this.rowActive = JSON.parse(JSON.stringify(row));
-		},
-		setComplete(){
-			this.rowActive.DateCompleted_At=new Date().toISOString()
-			console.log('setComplete',this.rowActive.DateCompleted_At);
-			this.setResult()
+			this.$set(this.rowActive, "state", state);
 		},
 		beforeEditNotes(row){
 			row.isEdit = true
@@ -220,28 +222,31 @@ export default {
 		},
 		setNotes(row){
 			this.rowActive = JSON.parse(JSON.stringify(row));
-			this.rowActive.Notes = this.InputNotes
+			this.rowActive.Notes = this.InputNotes;
+			this.$set(this.rowActive, "state", 0);
 			this.setResult()
 		},
-		setResult() {
-			setInspectionList( this.rowActive.InspectId, {
-				dateCompleted: this.formatTime(this.rowActive.DateCompleted_At),
-				notes: this.rowActive.Notes,
-			}).then(response => {
-				if ( response.statusCode == 20000 ) {
-					this.$message({
-						message: "提交成功",
-						type: "success",
-					});
+		async setResult() {
+			return new Promise(resolve => {
+				setInspectionList(this.rowActive.InspectId, {
+					state: this.rowActive.state,
+					notes: this.rowActive.Notes || null,
+				}).then(response => {
+					if ( response.statusCode == 20000 ) {
+						this.$message({
+							message: "提交成功",
+							type: "success",
+						});
+						this.getList();
+						resolve();
+					} 
+				}).catch(err => {
+					// console.log(err);
 					this.getList();
-				} 
-			}).catch(err => {
-				// console.log(err);
-				this.getList();
+				})
 			})
-		},
-		
-	},
+		}
+	}
 };
 </script>
 
