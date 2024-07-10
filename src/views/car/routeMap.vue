5d8c5e1aa3de273b9b8bfc5f3d0af5ab1e3c935f
lancelin <lancelin@bim-group.com>
2024-07-10 20:48:28 +0800

    change [巡視路線(車輛)] [巡視路線(分隊)] 調整「其他類型案件」顯示標籤

--- src/views/car/routeMap.vue
+++ src/views/car/routeMap.vue
@@ -916,14 +916,25 @@ export default {
 			this.dataLayer.case.addGeoJson(geoJSON);
 			this.dataLayer.case.setStyle(feature => {
 				const caseName = feature.getProperty("caseName");
+				const type = feature.getProperty("type");
 				const caseFilter = this.options.caseMap.filter(caseSpec => caseSpec.caseName.includes(caseName));
 
-				return { 
+				const styleObj = {
 					icon: { 
 						url: caseFilter.length == 0 ? '/assets/icon/icon_blue.png' : caseFilter[0].icon,
 						scaledSize: new google.maps.Size(30, 30),
 					}
 				};
+				
+				if(type) {
+					styleObj.label = { 
+						text: type, 
+						color: type == 1999 ? '#DC143C' : '#67C23A',
+						fontWeight: 'bold'
+					};
+				}
+
+				return styleObj;
 			})
 		},
 		caseFilter() { 
